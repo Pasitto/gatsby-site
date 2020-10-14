f6c7b1abf74f4ee6d57f473190ad827b41c9a761
guitarswp <guitarswp@gmail.com>
2020-10-14 23:57:01 +0700

    change homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <center><h1>Hello NCTzen </h1></center>
+    <p>Welcome to Neo Culture Technology 2020.</p>
+    <p>NCT in the house.</p>
+    <div >
+      <img src = "https://i1.wp.com/dudeplace.co/wp-content/uploads/2020/10/nct-dream-ot71.jpeg?resize=2048%2C1380&ssl=1" />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
   </Layout>
